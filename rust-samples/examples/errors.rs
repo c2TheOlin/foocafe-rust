f176e9938adbeb656d4434375da87d4ac3d46c94
Colin <colin.ingram@bimobject.com>
2019-09-12 23:22:00 +0200

    Options example

--- rust-samples/examples/errors.rs
+++ rust-samples/examples/errors.rs
@@ -6,7 +6,7 @@ fn main() {
     let result = do_something(result);
     println!("{}", result.unwrap());
 
-    do_something_else(fail);
+    do_something_bad(fail);
     println!("{}", result.unwrap());
 }
 
@@ -17,6 +17,6 @@ fn do_something(val: Option<i32>) ->  Option<i32> {
   }
 }
 
-fn do_something_else(val: Option<i32>) {
+fn do_something_bad(val: Option<i32>) {
      val.unwrap(); // Only catch is unwrap
 }
