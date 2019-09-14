1823cd3672e43569c153ff783f2e5412bbde1b52
Colin <colin.ingram@bimobject.com>
2019-09-14 17:12:22 +0200

    add result sample

--- rust-samples/examples/errors.rs
+++ rust-samples/examples/errors.rs
@@ -1,25 +1,32 @@
+use std::io;
+use std::error::Error;
 
-// Option<T>
-fn main() {
+// 
+fn main()  -> Result<(), Box<dyn Error>> {
+
+    //Option<T>
     let result = Some(5); // some method value
     let fail = None;
     let result = do_something(result);
     println!("{}", result.unwrap_or_else(|| 666));
 
     println!("{}", fail.unwrap_or_else(|| 666));
-    do_something_bad(fail);
+   // fail.unwrap(); // Only catch is unwrap
+
+    // Result <T, E>
+    // Match
+    let result = match do_something_else() {
+        Ok(v) => Ok(v),
+        Err(e) => Err(e)
+    };
+
+    // but this can be shorthanded to 
+    let result = do_something_else()?;
+    println!("{:?}", result);
+    Ok(())
 }
 
-// Result
-fn main() {
-    let result = Some(5); // some method value
-    let fail = None;
-    let result = do_something(result);
-    println!("{}", result.unwrap_or_else(|| 666));
 
-    println!("{}", fail.unwrap_or_else(|| 666));
-    do_something_bad(fail);
-}
 
 
 fn do_something(val: Option<i32>) ->  Option<i32> {
@@ -29,6 +36,6 @@ fn do_something(val: Option<i32>) ->  Option<i32> {
   }
 }
 
-fn do_something_bad(val: Option<i32>) {
-     val.unwrap(); // Only catch is unwrap
+fn do_something_else() -> Result<i32, io::Error> {
+    return Result::Err(io::Error::from(io::ErrorKind::ConnectionAborted))
 }
