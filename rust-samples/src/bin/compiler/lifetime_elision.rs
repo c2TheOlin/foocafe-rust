d51641a1e7b5db979b9a71065269f77a8f036be3
Colin <colin.ingram@bimobject.com>
2019-09-17 00:06:46 +0200

    Add unit tests

--- rust-samples/src/bin/compiler/lifetime_elision.rs
+++ rust-samples/src/bin/compiler/lifetime_elision.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct City {
     pub size_in_sqm: u32
 }
@@ -18,6 +19,7 @@ impl City {
 
 // since either city can be returned the input references must have the same lifetime to show relation
  pub fn compare_size_with_lifetimes<'a>(city: &'a City, city2: &'a City) -> (&'a City) {
+    do_something_with_other_cities(city, city2);
     if &city.size_in_sqm <= &city2.size_in_sqm {
             city2
         } else {
@@ -25,7 +27,39 @@ impl City {
         }
 }
 
- pub fn do_something_with_cities<'a, 'b, 'c, T>(city: &'a T, city2: &'b T, city3: &'c T) -> (&'b T) {
+ pub fn do_something_with_cities<'a, 'b, 'c>(city: &'a City, city2: &'b City, city3: &'c City) -> (&'b City) {
     //do something with other cities but retrun city2
     city2
+}
+
+fn do_something_with_other_cities<'a, 'b>(city: &'a City,  city2: &'a City) -> u32 {
+     city.size_in_sqm + city2.size_in_sqm
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn compare_sizes_returns_largest_city()
+    {
+        let city1 = City::new(2);
+        let city2 = City::new(4);
+
+        let result = compare_size_with_lifetimes(&city1, &city2);
+
+        assert_eq!(city2, *result);
+    }
+
+    
+    #[test]
+    fn do_something_with_other_cities_combines_sizes()
+    {
+        let city1 = City::new(2);
+        let city2 = City::new(4);
+
+        let result = do_something_with_other_cities(&city1, &city2);
+
+        assert!(result == 6);
+    }
 }
